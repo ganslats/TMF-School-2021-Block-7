3fc5da186938b2164319b7c30801a26eae83195b
Thomas Ganslandt <ganslan@gmail.com>
2021-06-27 19:06:18 +0200

    Übung 2 hinzugef+gt. Installation um Package gridExtra erweitert

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-packages <- c("leaflet", "ggmap", "readr", "tidyr", "ggplot2", "dplyr", "DBI", "RSQLite", "stringr", "icd.data", "lubridate", "zoo", "forcats", "grid", "gtable", "readr", "dplyr", "tidyr", "leaflet", "tidygeocoder", "sf", "ggmap")
+packages <- c("leaflet", "ggmap", "readr", "tidyr", "ggplot2", "dplyr", "DBI", "RSQLite", "stringr", "icd.data", "lubridate", "zoo", "forcats", "grid", "gridExtra", "gtable", "readr", "dplyr", "tidyr", "leaflet", "tidygeocoder", "sf", "ggmap")
 install.packages(setdiff(packages, rownames(installed.packages())))
